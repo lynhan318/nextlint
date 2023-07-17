1911da0ea265a1914d7451ffa6fccfa9d21d52cd
lynhan318 <lythanhnhan27294@gmail.com>
2023-07-17 23:39:33 +0700

    feat: scroll to flash menu

--- packages/svelte/src/lib/plugins/slash/Slash.svelte
+++ packages/svelte/src/lib/plugins/slash/Slash.svelte
@@ -8,6 +8,8 @@
   import {onDestroy, onMount} from 'svelte';
 
   export let props: SuggestionProps<SlashMenuItem>;
+  const SCROLL_HEIGHT = 400;
+  const ELEMENT_HEIGHT = 54;
 
   const editor = props.editor;
   $: query = props.query;
@@ -46,23 +48,34 @@
     onSelect(selectedIndex);
   };
 
-  const onActiveChange = (element: CustomEvent<HTMLElement>) => {
-    const clientRect = element.detail.getBoundingClientRect();
-    console.log('clientRect', clientRect);
-    console.log('scrollHeight', element.detail.parentElement?.scrollTop);
+  const getElementBounds = (parent: HTMLElement, element: HTMLElement) => {
+    const parentRect = parent.getBoundingClientRect();
+    const clientRect = element.getBoundingClientRect();
+    const viewPort = [parent.scrollTop, parent.scrollTop + SCROLL_HEIGHT + 48];
+
+    if (clientRect.top > viewPort[0] && clientRect.top < viewPort[1]) {
+      return undefined;
+    }
+
+    requestAnimationFrame(() => {
+      parent.scrollTo({
+        top: viewPort[1],
+        behavior: 'smooth'
+      });
+    });
+  };
+
+  const onActiveChange = (element: CustomEvent<HTMLElement>, idx: number) => {
     //max slash menu scroll is 400
     const parent = element.detail.parentElement!;
-    if (clientRect.y < 0) {
+    const elementHeight = (idx + 1) * ELEMENT_HEIGHT;
+    if (
+      elementHeight < parent.scrollTop ||
+      elementHeight > parent.scrollTop + SCROLL_HEIGHT
+    ) {
       requestAnimationFrame(() => {
         parent.scrollTo({
-          top: 400,
-          behavior: 'smooth'
-        });
-      });
-    } else if (clientRect.y > 400) {
-      requestAnimationFrame(() => {
-        parent.scrollTo({
-          top: parent.scrollTop + 300,
+          top: Math.max(elementHeight - ELEMENT_HEIGHT, 0),
           behavior: 'smooth'
         });
       });
@@ -70,7 +83,6 @@
   };
 
   const onSelect = (idx: number) => {
-    console.log('onSelect', idx);
     const item = menus[idx];
     if (item) {
       props.command(item);
@@ -86,10 +98,10 @@
       No results
     </p>
   {:else}
-    <div class="extension scroll">
+    <div class="extension scroll" style="max-height:{SCROLL_HEIGHT}px">
       {#each menus as item, idx}
         <Item
-          on:active={onActiveChange}
+          on:active={event => onActiveChange(event, idx)}
           text={item.title}
           icon={item.icon}
           description={item.description}
@@ -107,7 +119,6 @@
   }
   .wrapper {
     width: 336px;
-    max-height: 400px;
     height: 100%;
     display: flex;
     flex-direction: row;
