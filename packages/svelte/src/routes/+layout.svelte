98d817c55de1f484239700a5ddc8d075545b9c66
Nhân Lý <nhanly@Nhans-MacBook-Pro.local>
2023-11-14 00:47:15 +0700

    save

---
+++ packages/svelte/src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import './app.scss';
+  import {writable} from 'svelte/store';
+  import {setContext} from 'svelte';
+  import type {Editor} from '@tiptap/core';
+
+  import {EditorTheme} from '$lib';
+
+  import Devtool from './$components/Devtool.svelte';
+
+  const editor = writable<Editor>();
+  setContext('editor', editor);
+</script>
+
+<div class="relative flex min-h-screen flex-col px-6">
+  <header
+    class="h-[60px] sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
+  >
+    <Devtool />
+  </header>
+  <main class="max-w-6xl mx-auto">
+    <EditorTheme>
+      <slot />
+    </EditorTheme>
+  </main>
+</div>
--- packages/svelte/src/routes/+page.svelte
+++ packages/svelte/src/routes/+page.svelte
@@ -4,22 +4,10 @@
   import SvelteEditor from '$lib/Editor.svelte';
 
   import showcaseContent from './sveltor.json';
+  import {getContext, setContext} from 'svelte';
+  import type {Writable} from 'svelte/store';
 
-  import Devtool from './_components/Devtool.svelte';
-  import {EditorTheme} from '$lib';
-
-  let editor: Editor;
-  let theme: 'dark' | 'light' = 'dark';
-
-  const changeTheme = () => {
-    theme = theme === 'light' ? 'dark' : 'light';
-    document.body.classList.toggle('dark', theme === 'dark');
-  };
-
-  const onSave = () => {
-    const json = editor.getJSON();
-    localStorage.setItem('editor', JSON.stringify(json));
-  };
+  const editor: Writable<Editor> = getContext('editor');
 
   const submitPromt = async (prompt: string) => {
     const {data} = await fetch('https://api.nextlint.com/graphql', {
@@ -45,55 +33,18 @@
   };
 </script>
 
-<div class={'w-full p-4 mx-auto'} data-theme={theme}>
-  <EditorTheme {theme}>
-    <SvelteEditor
-      content={showcaseContent}
-      placeholder="Press 'space' GPT support, type '/' for help"
-      onCreated={createdEditor => {
-        editor = createdEditor;
-      }}
-      onChange={nextEditor => {
-        editor = nextEditor;
-      }}
-      plugins={{
-        selectImage: {
-          handleUpload,
-          unsplash: {
-            accessKey: 'omv67BHUb-gbDEbf9UwFsvGbKdQHwnreJPAzgI0Mz5I'
-          }
-        },
-        gpt: {query: submitPromt}
-      }}
-    />
-  </EditorTheme>
-</div>
-{#if editor}
-  <Devtool {editor}>
-    <button on:click={changeTheme}>change theme</button>
-  </Devtool>
-{/if}
-
-<style lang="scss">
-  .container {
-    display: flex;
-    flex-direction: row;
-  }
-
-  .preview {
-    max-width: 50%;
-    margin: auto;
-    margin-top: 120px;
-    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-    padding: 32px;
-    border-radius: 8px;
-  }
-  .editor {
-    max-width: 800px;
-    width: 100%;
-    margin: auto;
-  }
-  [data-theme='dark'] {
-    background: #1e1e1e;
-  }
-</style>
+<SvelteEditor
+  content={showcaseContent}
+  placeholder="Press 'space' GPT support, type '/' for help"
+  onCreated={editor.set}
+  onChange={editor.set}
+  plugins={{
+    selectImage: {
+      handleUpload,
+      unsplash: {
+        accessKey: 'omv67BHUb-gbDEbf9UwFsvGbKdQHwnreJPAzgI0Mz5I'
+      }
+    },
+    gpt: {query: submitPromt}
+  }}
+/>
