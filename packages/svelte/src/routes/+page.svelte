49d4d2d96a0a380d79f462bf75d1467a15ebe422
Kevin <lythanhnhan27294@gmail.com>
2025-03-09 13:39:17 +0700

    switch to bun

--- packages/svelte/src/routes/+page.svelte
+++ packages/svelte/src/routes/+page.svelte
@@ -1,41 +1,41 @@
 <script lang="ts">
-  import type {Editor} from '@tiptap/core';
+import type { Editor } from "@tiptap/core";
 
-  import SvelteEditor from '$lib/Editor.svelte';
+import SvelteEditor from "$lib/Editor.svelte";
 
-  import {getContext, onDestroy} from 'svelte';
-  import type {Writable} from 'svelte/store';
-  import {svelteEditorToHtml} from '$lib/helpers';
-  import {debounce} from 'radash';
+import { getContext, onDestroy } from "svelte";
+import type { Writable } from "svelte/store";
+import { svelteEditorToHtml } from "$lib/helpers";
+import { debounce } from "radash";
 
-  const editor: Writable<Editor> = getContext('editor');
+const editor: Writable<Editor> = getContext("editor");
 
-  const submitPromt = async (prompt: string) => {
-    return `An unofficial, community-led Svelte port of shadcn/ui. We are not affiliated with shadcn, but we did get his blessing before creating a Svelte version of his work. This project was born out of the need for a similar project for the Svelte ecosystem.
+const submitPromt = async (prompt: string) => {
+	return `An unofficial, community-led Svelte port of shadcn/ui. We are not affiliated with shadcn, but we did get his blessing before creating a Svelte version of his work. This project was born out of the need for a similar project for the Svelte ecosystem.
 This is NOT a component library. It's a collection of re-usable components that you can copy and paste or use the CLI to add to your apps.`;
-  };
+};
 
-  let html;
-  const toHtml = async () => {
-    html = await svelteEditorToHtml($editor);
-  };
+let html;
+const toHtml = async () => {
+	html = await svelteEditorToHtml($editor);
+};
 
-  const handleUpload = async (file: File) => {
-    const blob = new Blob([file]);
-    const previewUrl = URL.createObjectURL(blob);
-    return previewUrl;
-  };
-  const saveDraft = debounce({delay: 500}, (editor: Editor) => {
-    const jsContent = editor.getJSON();
-    localStorage.setItem('draft', JSON.stringify(jsContent));
-  });
-  const dispose = editor.subscribe(updated => {
-    if (updated) {
-      saveDraft(updated);
-    }
-  });
+const handleUpload = async (file: File) => {
+	const blob = new Blob([file]);
+	const previewUrl = URL.createObjectURL(blob);
+	return previewUrl;
+};
+const saveDraft = debounce({ delay: 500 }, (editor: Editor) => {
+	const jsContent = editor.getJSON();
+	localStorage.setItem("draft", JSON.stringify(jsContent));
+});
+const dispose = editor.subscribe((updated) => {
+	if (updated) {
+		saveDraft(updated);
+	}
+});
 
-  onDestroy(dispose);
+onDestroy(dispose);
 </script>
 
 <div class="mt-10">
